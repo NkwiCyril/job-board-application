8d19015bcd6486eb7aad8890a4ebcc3a9bfd54c5
NkwiCyril <akinimbomnkwi@gmail.com>
2024-05-01 07:22:28 +0100

    changed usertype datatype to enum

--- database/migrations/0001_01_01_000000_create_user_table.php
+++ database/migrations/0001_01_01_000000_create_user_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('usertype');
+            $table->enum('usertype',['seeker','company']);
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password')->nullable();
