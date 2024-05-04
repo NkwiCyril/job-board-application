345b8d97eecc9b72a9017440761052418cc836d3
NkwiCyril <akinimbomnkwi@gmail.com>
2024-05-04 03:20:24 +0100

    partials created as components and added to home page

---
+++ resources/views/partials/usertype.blade.php
@@ -0,0 +1,29 @@
+<h3 class="font-semibold text-gray-900 text-base">Register As</h3>
+<ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex white:bg-gray-700 white:border-gray-600 dark:text-white">
+  <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
+    <div class="flex items-center ps-3">
+      <input required id="usertype" type="radio" value="company" name="usertype" class="w-4 h-4 text-[#4ba198] bg-gray-100 border-gray-300 focus:ring-[#4ba198] dark:focus:ring-[#4ba198] white:ring-offset-gray-700 white:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-300 ">
+      <label for="usertype" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-900">Company</label>
+    </div>
+  </li>
+  <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
+    <div class="flex items-center ps-3">
+      <input required id="usertype" type="radio" value="seeker" name="usertype" class="w-4 h-4 text-[#4ba198] bg-gray-100 border-gray-300 focus:ring-[#4ba198] dark:focus:ring-[#4ba198] white:ring-offset-gray-700 white:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-300 ">
+      <label for="usertype" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-900">Opportunity Seeker</label>
+    </div>
+  </li>
+</ul>
+
+<script>
+  document.querySelectorAll('#usertype').forEach((radio) => {
+    radio.addEventListener('change', (e) => {
+      if (e.target.value === 'company') {
+        document.getElementById('category').classList.add('hidden');
+        document.getElementById('category').attributes.required = false;
+      } else {
+        document.getElementById('category').classList.remove('hidden');
+        document.getElementById('category').attributes.required = true;
+      }
+    });
+  });
+</script>
