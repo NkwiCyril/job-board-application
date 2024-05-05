6911edf0ae16c6029bf9fbee49d0f863c3417fa5
NkwiCyril <akinimbomnkwi@gmail.com>
2024-05-06 00:01:21 +0100

    partials for company, guest and seeker views

---
+++ resources/views/partials/company_page.blade.php
@@ -0,0 +1,37 @@
+<body class="bg-white">
+<header class="absolute inset-x-0 top-0 z-50">
+  <nav class="flex items-center justify-between px-6 py-3 lg:px-8" aria-label="Global">
+    <div class="flex lg:flex-1">
+      <a href="{{route('pages.home')}}" class="-m-1.5 p-1.5">
+        <span class="sr-only">Seeka</span>
+        <img class="h-12 w-auto" src="/seeka_logo.png" alt="seeka logo">
+      </a>
+    </div>
+    <!-- <div class="flex lg:hidden">
+        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+          <span class="sr-only">Open main menu</span>
+          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </button>
+      </div> -->
+    <div class="{{--hidden--}} items-center justify-center gap-5 lg:flex lg:flex-1 lg:justify-end sm:flex">
+      <a href="{{route('pages.create_opportunity')}}" title="Post an opportunity" class=" text-decoration-none text-xl font-semibold leading-6 transition ease-in-out text-[#4ba198] hover:text-[#377b74]">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+          <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
+        </svg>
+      </a>
+      <a href="{{route('pages.create_opportunity')}}" title="Unpublished" class=" text-decoration-none text-xl font-semibold leading-6 transition ease-in-out text-[#4ba198] hover:text-[#377b74]">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+          <path d="M9.97.97a.75.75 0 0 1 1.06 0l3 3a.75.75 0 0 1-1.06 1.06l-1.72-1.72v3.44h-1.5V3.31L8.03 5.03a.75.75 0 0 1-1.06-1.06l3-3ZM9.75 6.75v6a.75.75 0 0 0 1.5 0v-6h3a3 3 0 0 1 3 3v7.5a3 3 0 0 1-3 3h-7.5a3 3 0 0 1-3-3v-7.5a3 3 0 0 1 3-3h3Z" />
+          <path d="M7.151 21.75a2.999 2.999 0 0 0 2.599 1.5h7.5a3 3 0 0 0 3-3v-7.5c0-1.11-.603-2.08-1.5-2.599v7.099a4.5 4.5 0 0 1-4.5 4.5H7.151Z" />
+        </svg>
+      </a>
+      @include('partials.profile')
+    </div>
+  </nav>
+</header>
+  <div class="intro text-center prose lg:prose-xl mx-auto mt-20 p-10">
+    <h1>all opps will display here for to view {{Auth::user()->name}}</h1>
+  </div>
+</body>
--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -1,26 +1,27 @@
-<div class="relative inline-block text-left">
+<div class="relative inline-block items text-left">
   <div>
-    <button type="button" id="profile_btn" class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
-      <span class="flex items-center gap-1 text-xl font-semibold leading-6 text-gray-900 transition ease-in-out hover:text-[#4ba198]">
-        Profile
-        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-        </svg>
+    <button type="button" id="profile_btn" class="inline-flex items-center text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
+      <span class="flex items-center text-xl font-semibold leading-6 text-gray-900 transition ease-in-out hover:text-[#4ba198]">
+        <div class="flex items-center w-fit">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </div>
+        </div>
       </span>
     </button>
   </div>
 
-  <div id="profile_menu" class="hidden absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+
+  <div id="profile_menu" class="hidden absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" tabindex="-1">
     <div class="py-1" role="none">
-      <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-      <a href="{{route('pages.profile', [$id = Auth::user()->id])}}" class="text-gray-700 flex items-center gap-2 px-4 py-2 text-lg" role="menuitem" tabindex="-1" id="menu-item-0">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-          <path fill-rule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clip-rule="evenodd" />
+      <a href="{{route('pages.profile', [$id = Auth::user()->id])}}" class="text-gray-800 hover:text-[#4ba198] flex items-center gap-2 px-4 py-2 text-lg " tabindex="-1">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 hover:text-[#4ba198]">
+          <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
         </svg>
-        {{Auth::user()->name}} Profile
+        Your Profile
       </a>
       <form method="POST" action="{{route('auth.logout')}}" role="none">
-        <button type="submit" class="flex gap-2 items-center text-gray-700 w-full px-4 py-2 text-left text-lg" role="menuitem" tabindex="-1" id="menu-item-3">
+        <button type="submit" class="flex gap-2 items-center text-gray-800 hover:text-[#4ba198] w-full px-4 py-2 text-left text-lg hover:underline" role="menuitem" tabindex="-1" id="menu-item-3">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
             <path fill-rule="evenodd" d="M7.5 3.75A1.5 1.5 0 0 0 6 5.25v13.5a1.5 1.5 0 0 0 1.5 1.5h6a1.5 1.5 0 0 0 1.5-1.5V15a.75.75 0 0 1 1.5 0v3.75a3 3 0 0 1-3 3h-6a3 3 0 0 1-3-3V5.25a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3V9A.75.75 0 0 1 15 9V5.25a1.5 1.5 0 0 0-1.5-1.5h-6Zm10.72 4.72a.75.75 0 0 1 1.06 0l3 3a.75.75 0 0 1 0 1.06l-3 3a.75.75 0 1 1-1.06-1.06l1.72-1.72H9a.75.75 0 0 1 0-1.5h10.94l-1.72-1.72a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
           </svg>
@@ -29,15 +30,14 @@
       </form>
     </div>
   </div>
-</div>
 
-<script>
-  document.getElementById('profile_btn').addEventListener('click', function() {
-    var profileMenu = document.getElementById('profile_menu');
-    if (profileMenu.classList.contains('hidden')) {
-      profileMenu.classList.remove('hidden')
-    } else {
-      profileMenu.classList.add('hidden');
-    }
-  });
-</script>
+  <script>
+    document.getElementById('profile_btn').addEventListener('click', function() {
+      var profileMenu = document.getElementById('profile_menu');
+      if (profileMenu.classList.contains('hidden')) {
+        profileMenu.classList.remove('hidden')
+      } else {
+        profileMenu.classList.add('hidden');
+      }
+    });
+  </script>
