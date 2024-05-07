e947c37f76a3a412ed8af3196809ab0286db5e22
NkwiCyril <akinimbomnkwi@gmail.com>
2024-05-07 14:29:31 +0100

    changes made on the navigation bar and items

--- resources/views/partials/opp_section.blade.php
+++ resources/views/partials/opp_section.blade.php
@@ -1,16 +1,17 @@
 <div>
-  <div class="flex justify-between items-center">
+  <div class="flex-1 justify-between items-center">
     <div class="flex items-center justify-center gap-2">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-        <path d="M5.625 3.75a2.625 2.625 0 1 0 0 5.25h12.75a2.625 2.625 0 0 0 0-5.25H5.625ZM3.75 11.25a.75.75 0 0 0 0 1.5h16.5a.75.75 0 0 0 0-1.5H3.75ZM3 15.75a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75ZM3.75 18.75a.75.75 0 0 0 0 1.5h16.5a.75.75 0 0 0 0-1.5H3.75Z" />
-      </svg>
-      <h2 class="text-2xl font-bold">Opportunity Listings</h2>
+      <input autocomplete type="text" name="search" id="search_field" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-customColor focus:border-customColor block w-2/5 p-2.5 white:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-600 dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search by job title, category or company">
+      <select name="filter" id="filter" class="text-gray-900 text-sm rounded-lg focus:ring-customColor p-2.5 focus:border-customColor">
+        <option value="">Filter</option>
+        <option value="1">Jobs</option>
+        <option value="2">Internships</option>
+        <option value="3">Volunteerism</option>
+      </select>
     </div>
-    <input type="text" name="search" id="search_field" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-customColor focus:border-primary-600 block w-2/5 p-2.5 white:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-600 dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search by job title, category or company">
   </div>
 
   <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-
     @foreach ($opportunities as $opportunity)
     <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
       <div class="flex items-end justify-end h-56 w-full bg-cover bg-[url({{$opportunity['image_url']}})]">
@@ -20,7 +21,24 @@
         <a href="" class=" text-decoration-none no-underline">
           <h3 class="text-gray-700 uppercase hover:text-customColor font-medium">{{$opportunity['title']}}</h3>
         </a>
-        <span class="text-gray-500 mt-2 text-sm">{{$opportunity['description']}}</span>
+        <div class=" flex-col justify-end">
+          <p class="text-gray-500 mt-2 text-sm">
+            @php
+              $desc = $opportunity['description'];
+              if (($desc) > 50) {
+                $desc = substr($desc, 0, 50). '...';
+              }
+            @endphp
+            {{$desc}}
+          </p>
+          @if ($opportunity['category_id'] === 1)
+          <span class="inline-flex items-center rounded-md bg-green-50 mt-2 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Job</span>
+          @elseif ($opportunity['category_id'] === 2)
+          <span class="inline-flex items-center rounded-md bg-blue-50 mt-2 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">Internship</span>
+          @elseif ($opportunity['category_id'] === 3)
+          <span class="inline-flex items-center rounded-md bg-yellow-50 px-2 mt-2  py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">Volunteerism</span>
+          @endif
+        </div>
       </div>
     </div>
     @endforeach
--- resources/views/partials/seeker_page.blade.php
+++ resources/views/partials/seeker_page.blade.php
@@ -1,5 +1,5 @@
 <body class="bg-white">
-  <header class="absolute inset-x-0 top-0 z-50">
+  <header class="absolute inset-x-0 top-0 z-50 border-bottom border-customColorDark">
     <nav class="flex items-center justify-between px-10 py-3 lg:px-10" aria-label="Global">
       <div class="flex lg:flex-1">
         <a href="{{route('pages.home')}}" class="-m-1.5 p-1.5">
@@ -15,8 +15,11 @@
           </svg>
         </button>
       </div> -->
-      <div class="{{--hidden--}} gap-2 lg:flex items-center lg:flex-1 lg:justify-end sm:flex">
-        <a href="{{route('pages.applications')}}" title="My Applications" class="text-xl font-semibold leading-6 text-gray-900 transition ease-in-out hover:text-[#4ba198]">My Seeks</a>
+      <div class="{{--hidden--}} gap-6 lg:flex items-center lg:flex-1 lg:justify-end sm:flex">
+        <a href="{{route('pages.applications')}}" title="My Applications" class=" hover:no-underline text-xl font-semibold leading-6 text-gray-900 transition ease-in-out hover:text-customColor">My Seeks</a>
+        <svg class="h-8 w-8 hover:text-customColor cursor-pointer" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+        </svg>
         @include('partials.profile')
       </div>
     </nav>
@@ -25,3 +28,9 @@
     @include('partials.opp_section')
   </div>
 </body>
+
+<script>
+
+  
+
+</script>
