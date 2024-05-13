68a88329bb0d08e673b7bb9d9ea6b442bec49a70
NkwiCyril <akinimbomnkwi@gmail.com>
2024-05-13 21:27:36 +0100

    configuration for gmail notifications

--- config/mail.php
+++ config/mail.php
@@ -104,8 +104,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@seeka.com'),
+        'name' => env('MAIL_FROM_NAME', 'Seeka'),
     ],
 
 ];
