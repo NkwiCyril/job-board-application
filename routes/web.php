d45d5166ccd61b334b89d33707851090547b126e
NkwiCyril <akinimbomnkwi@gmail.com>
2024-05-13 06:30:36 +0100

    deleted some unecessary routes

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\ApplicationController;
 
 // login routes
 Route::get('login', [LoginController::class, 'login'])->name('auth.login');
-Route::get('logout', [LoginController::class, 'logout'])->name('auth.logout');
+Route::post('logout', [LoginController::class, 'logout'])->name('auth.logout');
 Route::post('login', [LoginController::class, 'authenticate'])->name('auth.authenticate');
 
 
@@ -40,20 +40,21 @@ Route::get('profile/{id}', [ProfileController::class, 'view_profile'])->name('pa
 Route::patch('profile/edit/{id}', [ProfileController::class, 'edit_profile'])->name('pages.edit_profile');
 
 // routes concerned with opportunities
-Route::patch('opportunity/edit/{id}', [OpportunityController::class, 'edit_opportunity'])->name('pages.edit_opportunity');
-Route::get('opportunity/delete/{id}', [OpportunityController::class, 'delete_opportunity'])->name('pages.delete_opportunity');
 Route::get('opportunity/create', [OpportunityController::class, 'create_opportunity'])->name('pages.create_opportunity');
 Route::post('opportunity/create', [OpportunityController::class,'store_opportunity'])->name('pages.store_opportunity');
+Route::get('opportunity/edit/{id}', [OpportunityController::class, 'edit_opportunity'])->name('pages.edit_opportunity');
+Route::put('opportunity/edit/{id}', [OpportunityController::class, 'update'])->name('pages.update');
+Route::get('opportunity/delete/{id}', [OpportunityController::class, 'delete_opportunity'])->name('pages.delete_opportunity');
+Route::get('opportunity/publish', [OpportunityController::class, 'all_published'])->name('pages.publish_opportunity');
 Route::get('opportunity/{id}', [OpportunityController::class, 'view_opportunity'])->name('pages.opportunity');
+Route::get('opportunity/publish/{id}', [OpportunityController::class, 'publish_opportunity'])->name('pages.publish');
+Route::get('opportunity/unpublish/{id}', [OpportunityController::class, 'unpublish_opportunity'])->name('pages.unpublish');
 
 
 // routes concerned with applications
 Route::get('applications', [ApplicationController::class, 'all_application'])->name('pages.applications');
-Route::patch('application/edit/{id}', [ApplicationController::class, 'edit_application'])->name('pages.edit_application');
-Route::get('application/delete/{id}', [ApplicationController::class, 'delete_application'])->name('pages.delete_application');
-Route::get('application/create', [ApplicationController::class, 'create_application'])->name('pages.create_application');
-Route::post('application/create/{id}', [ApplicationController::class,'store_application'])->name('pages.store_application');
-Route::get('application/{id}', [ApplicationController::class, 'view_application'])->name('pages.application');
+Route::post('application/submit/{opp_id}', [ApplicationController::class, 'send_application'])->name('pages.submit_application');
+Route::get('application/{id}', [ApplicationController::class, 'view_application_form'])->name('pages.application');
 
 // fallback 404 view in case of invalid route parameters
 Route::fallback(function() {
