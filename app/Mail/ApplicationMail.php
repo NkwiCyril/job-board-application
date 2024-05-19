026265d6898f8f45801e9223925f613a96cf3358
NkwiCyril <akinimbomnkwi@gmail.com>
2024-05-19 21:33:53 +0100

    new opportunity and application mails

--- app/Mail/ApplicationMail.php
+++ app/Mail/ApplicationMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ApplicationMail extends Mailable
+class ApplicationMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
--- app/Mail/NewOpportunityMail.php
+++ app/Mail/NewOpportunityMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NewOpportunityMail extends Mailable
+class NewOpportunityMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
